5923dc6a718daa522ef9a86faa4f84b0732463cf
alex-mat-s <alexandra.matveeva.s@yandex.ru>
2023-05-09 00:22:18 +0300

    Weighted Quick Union with Path Compressing

--- algorithms/union_find.py
+++ algorithms/union_find.py
@@ -53,14 +53,14 @@ class QuickUnion:
             data (list): user-defined data
             init_flag (bool): flag to create default set [0, 1, 2, ..]
             N (int): the length of the default set
-            improvement (False, "weighting", "compressing")
+            improvement (False, "weighting", "compressing", "all")
         """
         self.improvement = improvement
 
         if init_flag:
             # Set id of each object to itself.
             self.dis_set = [i for i in range(N)]
-            if self.improvement == "weighting":
+            if self.improvement == "weighting" or self.improvement == "all":
                 self.trees_size = [1] * N
         else:
             self.dis_set = []
@@ -74,7 +74,7 @@ class QuickUnion:
             data = f.readlines()
             # Set id of each object to itself.
             self.dis_set = [i for i in range(int(data[0]))]
-            if self.improvement == "weighting":
+            if self.improvement == "weighting" or self.improvement == "all":
                 self.trees_size = [1] * int(data[0])
             for line_id in range(1, len(data)):
                 num1, num2 = data[line_id].split()
@@ -83,7 +83,7 @@ class QuickUnion:
     def root(self: list, i: int):
         """Chase parent pointers until reach root."""
         while i != self.dis_set[i]:
-            if self.improvement == "compressing":
+            if self.improvement == "compressing" or self.improvement == "all": 
                 self.dis_set[i] = self.dis_set[self.dis_set[i]]
             i = self.dis_set[i]
         return i
@@ -109,10 +109,13 @@ class QuickUnion:
             p_root = self.root(p)
             q_root = self.root(q)
             
-            if self.improvement == "weighting":
+            if self.improvement == "weighting" or self.improvement == "all":
                 self.__weighted_union(p_root, q_root)
             else:
                 self.dis_set[p_root] = q_root
+        print("set:", self.dis_set)
+        if self.improvement == "weighting" or self.improvement == "all":
+            print("size:", self.trees_size)
 
                 
 if __name__ == '__main__':
@@ -139,5 +142,8 @@ if __name__ == '__main__':
     print("Quick Union:", data_QU.get_data())
     data_PCQU = QuickUnion(data_path=pth, improvement="compressing")
     print("Path compressed Quick Union:", data_PCQU.get_data())
+
+    data_WQUPC = QuickUnion(data_path=pth, improvement="all")
+    print("Weighted path compressed Quick Union:", data_WQUPC.get_data())
 
     
